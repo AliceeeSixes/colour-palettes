e049b236bf7f96e27cc120548c4f603704a97cde
AliceeeSixes <6d6perturn@gmail.com>
2025-01-15 18:50:22 +0000

    10% lighter/darker buttons, redo page layout

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ function newColour(rgb) {
     let green = colours[1];
     let blue = colours[2];
     let opposite = invert(red, green, blue);
+    let lighter = rgbLighten(red, green, blue);
+    let darker = rgbDarken(red, green, blue);
     red = `<input value="`+red+`" class="red" name="red" type="number" max="255"></input>`;
     green = `<input value="`+green+`" class="green" name="green" type="number" max="255"></input>`;
     blue = `<input value="`+blue+`" class="blue" name="blue" type="number" max="255"></input>`;
@@ -17,10 +19,27 @@ function newColour(rgb) {
 
     // Create new element
 
+    // Initialise html
+    let html = ``;
+
+    // Initialise new DOM element
     let $newColour = $(`<div></div>`).addClass("colour-tile").css("background-color",rgb);
-    let buttons = `<div class="colour-tile-buttons"><button class="add-opposite">Add opposite colour</button></div>`;
-    $newColour = $newColour.html(`<button class="colour-tile-delete">Remove</button><div class="colour-tile-text"><h1>`+rgb+`</h1><form><h2>Red: `+red+`</h2><h2>Green: `+green+`</h2><h2>Blue: `+blue+`</h2></form></div>`+buttons);
-    $($newColour).find(".add-opposite").css("background-color",opposite);
+
+    // HTML for buttons
+    let addOppositeButton = `<button class="add-opposite">Add opposite colour</button>`;
+    let addDarkerButton = `<button class="add-darker">Add 10% darker</button>`;
+    let addLighterButton = `<button class="add-lighter">Add 10% lighter</button>`;
+    let buttons = `<div class="colour-tile-buttons">`+addDarkerButton+addOppositeButton+addLighterButton+`</div>`;
+
+
+    html += `<button class="colour-tile-delete">Remove</button><div class="colour-tile-text"><h1>`+rgb+`</h1><form><h2>Red: `+red+`</h2><h2>Green: `+green+`</h2><h2>Blue: `+blue+`</h2></form></div>`+buttons;
+
+
+    $newColour.html(html);
+    // Colour buttons correctly
+    $($newColour).find(".add-opposite").css("background-color",opposite).css("color",rgb);
+    $($newColour).find(".add-lighter").css("background-color",lighter).css("color",opposite);
+    $($newColour).find(".add-darker").css("background-color",darker).css("color",opposite);
 
     // Colour Tiles update backgrounds
     $newColour.on("input",(event) => {
@@ -29,11 +48,15 @@ function newColour(rgb) {
         let green = $(container).find(".green").val();
         let blue = $(container).find(".blue").val();
         let opposite = invert(red, green, blue);
+        let lighter = rgbLighten(red, green, blue);
+        let darker = rgbDarken(red, green, blue);
         let rgb = `rgb(`+red+`,`+green+`,`+blue+`)`;
         container.css("background-color",rgb);
         let newText = `rgb(`+red+`, `+green+`, `+blue+`)`;
         $(container).find("h1").text(newText);
-        $(container).find(".add-opposite").css("background-color",opposite);
+        $(container).find(".add-opposite").css("background-color",opposite).css("color",rgb);
+        $(container).find(".add-lighter").css("background-color",lighter).css("color",opposite);
+        $(container).find(".add-darker").css("background-color",darker).css("color",opposite);
     });
 
 
@@ -42,7 +65,7 @@ function newColour(rgb) {
     $("#main__container-contents").append($newColour);
 }
 
-
+// Generate random RGB values
 function randomRGB() {
     let red = 1 + Math.floor(Math.random() * 255);
     let green = 1 + Math.floor(Math.random() * 255);
@@ -70,13 +93,39 @@ function invert(red, green, blue) {
     return(rgb);
 }
 
-$("#main__container-contents").on("click","button.add-opposite", (event) => {
+
+// Buttons create new tiles
+$("#main__container-contents").on("click","button", (event) => {
     let colour = $(event.target).css("background-color");
     newColour(colour);
 });
+
+
+// Lighten function
+
+function rgbLighten(red, green, blue) {
+    let newRed = Math.min(red/0.9, 255);
+    let newBlue = Math.min(blue/0.9, 255);
+    let newGreen = Math.min(green/0.9, 255);
+    let rgb = `rgb(`+newRed+`,`+newGreen+`,`+newBlue+`)`;
+    return(rgb);
+
+}
+
+// Darken function
+
+function rgbDarken(red, green, blue) {
+    let newRed = Math.max(red*0.9, 0);
+    let newBlue = Math.max(blue*0.9, 0);
+    let newGreen = Math.max(green*0.9, 0);
+    let rgb = `rgb(`+newRed+`,`+newGreen+`,`+newBlue+`)`;
+    return(rgb);
+
+}
 
 
 // Clear All Colours
 function clearColours() {
     $(".colour-tile").remove();
 }
+
